51d0fd01b2b0e0a373b5687dd0ad9065022b4787
Marcel-BK <MarcelBK@icloud.com>
2022-06-20 17:47:14 +0200

    create extras path structure

---
+++ src/controllers/extrasControllers.js
@@ -0,0 +1,7 @@
+export const getAllExtras = (req, res) => {};
+
+export const createNewExtra = (req, res) => {};
+
+export const deleteExtra = (req, res) => {};
+
+export const updateExtra = (req, res) => {};

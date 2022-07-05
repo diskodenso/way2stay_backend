28c6d59c1b8224bee0b6157753c9d6b3e82571da
Matthias Wieser <matthias.wieser.83@live.de>
2022-07-05 15:49:23 +0200

    add country field value pair to flat (missing update method)

--- src/controllers/flatsController.js
+++ src/controllers/flatsController.js
@@ -71,14 +71,14 @@ export const updateSingleFlat = async (req, res) => {
     const { flatId } = req.params;
     const {
       isActive, title, description, maxPersons, size, bedroom, bathroom, floor, extras, pets,
-      kids, categories, street, housenumber, postalcode, city, lat, lang, images,
+      kids, categories, street, housenumber, postalcode, city, country, lat, lang, images,
     } = req.body;
     const updatedFlat = {
       isActive, title, description, details: {
         maxPersons, size, bedroom, bathroom, floor, extras, pets, kids, categories,
       },
       location: {
-        street, housenumber, postalcode, city,
+        street, housenumber, postalcode, city, country
       },
       coordinates: {
         lat, lang,
